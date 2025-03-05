8a0f787b8b75a352d80ee319806b9ad34a0021a4
devalamin <alamin.hasibmd2017@gmail.com>
2025-03-05 10:14:36 +0300

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='flex justify-center text-blue-600 h-screen items-center text-3xl font-bold'>
+      <h4>Hello EveryOne</h4>
+    </div>
+  );
+};
+
+export default App;
